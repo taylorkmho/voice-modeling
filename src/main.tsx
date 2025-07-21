bb359033dfede039b85f73341796c2e8bad41abf
Taylor Ho <taylorkmho@gmail.com>
2025-07-21 12:09:36 -0700

    chore: default to dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "./components/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="system" storageKey="voice-demo-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="voice-demo-theme">
       <App />
     </ThemeProvider>
   </StrictMode>
